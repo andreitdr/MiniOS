02d7b1f52e4e6d773a815b6a464a52c646530e87
Tudor Andrei <tudor.andrei.707@gmail.com>
2023-10-08 22:09:38 +0300

    prepare_env_2.0

--- prepare_enviroment.sh
+++ prepare_enviroment.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 sudo apt update
 sudo apt upgrade
 
@@ -7,13 +5,6 @@ sudo apt install nasm make qemu qemu-system-i386
 
 sudo apt install wget bochs bochs-sdl bochsbios vgabios
 
-mkdir Downloads
-cd Downloads
-
 wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/open-watcom-2_0-c-linux-x64
-sudo chmod +x ./open-watcom-2_0-c-linux-x64
-sudo ./open-watcom-2_0-c-linux-x64
-rm ./open-watcom-2_0-c-linux-x64
-rm -fr ./Downloads
 
 sudo apt install gcc
