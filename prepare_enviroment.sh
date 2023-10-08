82f92d2bc745642a21eea9a0f8ba1c441a1e11ff
Tudor Andrei <tudor.andrei.707@gmail.com>
2023-10-08 21:24:10 +0300

    Created script to prepare the enviroment

---
+++ prepare_enviroment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install nasm make qemu qemu-system-i386
+
+sudo apt install wget bochs bochs-sdl bochsbios vgabios
+
+mkdir Downloads
+cd Downloads
+
+wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/open-watcom-2_0-c-linux-x64
+sudo chmod +x ./open-watcom-2_0-c-linux-x64
+sudo ./open-watcom-2_0-c-linux-x64
+rm ./open-watcom-2_0-c-linux-x64
+
