19f8d691d187263664fc53616f1cf1c3d8270070
Tudor Andrei <tudor.andrei.707@gmail.com>
2023-10-08 22:10:24 +0300

    added mtools to install

--- prepare_enviroment.sh
+++ prepare_enviroment.sh
@@ -7,4 +7,4 @@ sudo apt install wget bochs bochs-sdl bochsbios vgabios
 
 wget https://github.com/open-watcom/open-watcom-v2/releases/download/Current-build/open-watcom-2_0-c-linux-x64
 
-sudo apt install gcc
+sudo apt install gcc mtools
